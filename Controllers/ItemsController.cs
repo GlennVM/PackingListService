200438eb78d6955fbb63db61fade1c8d8c99ea7e
Glenn Van Mele <glennvanmele@gmail.com>
2016-08-14 17:12:06 +0200

    Commit

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,41 @@
+﻿using PackingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PackingList.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        private ItemRepository repo = null;
+
+        public ItemsController()
+        {
+            this.repo = new ItemRepository();
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            Item[] items = repo.GetAllItems().ToArray();
+            return Request.CreateResponse<IEnumerable<Item>>(HttpStatusCode.OK, items);
+        }
+
+        [HttpGet]
+        // GET /api/values/5
+        public HttpResponseMessage Get(int Id)
+        {
+            return Request.CreateResponse<String>(HttpStatusCode.OK, Id.ToString());
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post(Item item)
+        {
+            repo.AddItem(item);
+            return Request.CreateResponse<IEnumerable<Item>>(HttpStatusCode.OK, repo.GetAllItems().ToArray());
+        }
+    }
+}
